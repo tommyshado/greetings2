a072192c90783e55be223a47554477dc111c41eb
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-05-10 12:55:55 +0200

    first commit

---
+++ greetingApp.js
@@ -0,0 +1,72 @@
+function greetingsApp(countResult) {
+    let counter = countResult || 0;
+    let userName = {};
+    let msg = '';
+    // update user name
+    function updateUser(name) {
+        if (userName[name] !== undefined) {
+            userName[name] += 1;
+        } else {
+            userName[name] = 1;
+            counter += 1;
+        }
+    }
+    // greet user in three languages
+    function greetUserName(user, lang) {
+        if (user && lang === 'IsiXhosa') {
+            msg = `Molo ${user}.`;
+            return msg;
+        } 
+        if (user && lang === 'SiSotho') {
+            msg = `Dumela ${user}.`;
+            return msg;
+        }
+        if (user && lang === 'English') {
+            msg = `Hello ${user}.`;
+            return msg;
+        }
+    }
+    
+    function errorMessages(user, lang) {
+        msg = '';
+        if (user === '' && lang === null) {
+            msg = 'Please enter name and check language.';
+            return msg;
+        } 
+        if (user === '') {
+            msg = 'Please enter name.';
+            return msg;
+        }
+        if (lang === null) {
+            msg = 'Please check a language.';
+            return msg;
+        }
+        if (userName[user] >= 2) {
+            msg = `${user} has already been greeted.`;
+            return msg;
+        }
+        return '';
+    }
+
+    function greetingsCount() {
+        return counter;
+    }
+
+    function reset() {
+        counter = 0;
+        return counter;
+    }
+
+    function nameOfUser() {
+        return userName;
+    }
+
+    return {
+        greetUserName,
+        updateUser,
+        errorMessages,
+        greetingsCount,
+        reset,
+        nameOfUser
+    }
+}
---
+++ greetings.js
@@ -0,0 +1,42 @@
+const nameReference = document.querySelector('.nameString');
+const greetBtnRef = document.querySelector('.greetMeBtn');
+const greet = document.querySelector('#greet');
+const countOfGreet = document.querySelector('#counter');
+const resetBtn = document.querySelector('.resetBtn');
+const errorMessage = document.querySelector('#errorMsg');
+var countResult = 0;
+
+const greetApp = greetingsApp(countResult);
+
+if (localStorage['counter']) {
+    countResult = Number(localStorage['counter']);
+    countOfGreet.innerHTML = countResult;
+}
+
+greetBtnRef.addEventListener('click', function() {
+    const radioReferenceBtn = document.querySelector('.language:checked');
+
+    if (radioReferenceBtn) {
+        greet.innerHTML = greetApp.greetUserName(nameReference.value, radioReferenceBtn.value);
+        greet.classList.add('display');
+        greetApp.updateUser(nameReference.value);
+        countOfGreet.innerHTML = greetApp.greetingsCount();
+    }
+
+    errorMessage.innerHTML = greetApp.errorMessages(nameReference.value, radioReferenceBtn);
+    errorMessage.classList.add('danger');
+
+    localStorage.setItem('greetedNames', JSON.stringify(greetApp.nameOfUser()));
+
+    setTimeout(function() {
+        errorMessage.style.display = 'none';
+    }, 3000);
+
+    nameReference.value = '';
+});
+
+resetBtn.addEventListener('click', function() {
+    countOfGreet.innerHTML = greetApp.reset();
+    greet.innerHTML = '';
+    errorMessage.innerHTML = '';
+});
